368802db9d30f9d693afe51c7bda8037c56953f3
polochinoc <polochinoc@gmail.com>
2017-11-24 19:27:05 +0100

    en cours...

--- donnees_amap.sql
+++ donnees_amap.sql
@@ -5,7 +5,7 @@
 -- Tables définies par des couples/triplets de clefs étrangères
 DELETE FROM prevision_calendrier;
 DELETE FROM souscrire_a;
-DELETE FROM contenir;
+DELETE FROM contenir; 
 DELETE FROM appartenir_a;
 
 -- Tables référencées par les tables précédentes
@@ -27,7 +27,8 @@ commit;
 -- adresse
 
 INSERT INTO adresse VALUES (1 , 'France', 'Roquefort-sur-Soulzon', '12250', '12', 'rue des Baragnaudes');
-INSERT INTO adresse VALUES (2 , 'France', 'Bordeaux', '33800', '35', 'Pauline Kergomard');
+INSERT INTO adresse VALUES (2 , 'France', 'Bordeaux', '33800', '35', 'rue Pauline Kergomard');
+INSERT INTO adresse VALUES (3 , 'France', 'Talence', '33400', '1', 'avenue du Dr Albert Schweitzer');
 
 commit;
 
@@ -45,7 +46,8 @@ commit;
 
 -- foyer
 
-INSERT INTO foyer VALUES (1 , 2, 'Eirb', 'On brasse', 'eirb@enseirb-matmeca.fr', '0556565656');
+INSERT INTO foyer VALUES (1 , 3, 'Eirb', 'On brasse', 'eirb@enseirb-matmeca.fr', '0556565656');
+INSERT INTO foyer VALUES (2 , 2, 'Pizza-flat', 'Superbe villa', 'avolte001@enseirb-matmeca.fr', '0666666666');
 
 commit;
 
@@ -57,13 +59,48 @@ commit;
 
 -- livraison
 
-INSERT INTO livraison VALUES  (1, 2, 1, '2017-11-27 10:30:00');
+INSERT INTO livraison VALUES (1, 2, 1, '2017-11-27 10:30:00');
+
+commit;
 
 -- panier
 
+INSERT INTO panier VALUES (1, 'terroir occitan', 'contient un ensemble de produits traditionnels du Midi');
+INSERT INTO panier VALUES (2, 'terroir auvergnat', 'contient un ensemble de produits traditionnels d\'Auvergne');
+
+commit;
+
 -- denree
 
+-- Midi-Pyrénées
+INSERT INTO denree VALUES (1, 'foie gras', 'incontournable de la gastronomie occitane', 'g');
+INSERT INTO denree VALUES (2, 'haricot', 'Tarbais', 'g');
+INSERT INTO denree VALUES (3, 'raisin', 'Chasselas', 'g');
+INSERT INTO denree VALUES (4, 'melon', 'du Quercy', 'u');
+INSERT INTO denree VALUES (5, 'prune', 'Reine-Claude', 'g');
+INSERT INTO denree VALUES (6, 'noix', 'Périgord', 'L');
+INSERT INTO denree VALUES (7, 'roquefort', 'issue des caves Papillon', 'g');
+INSERT INTO denree VALUES (8, 'bleu', 'des Causses', 'g');
+INSERT INTO denree VALUES (9, 'Laguiole', '', 'g');
+INSERT INTO denree VALUES (10, 'Rocamadour', '', 'g');
+
+-- Auvergne
+INSERT INTO denree VALUES (11, 'Cantal', '', 'g');
+INSERT INTO denree VALUES (12, 'Salers', '', 'g');
+INSERT INTO denree VALUES (13, 'Saint-Nectaire', '', 'g');
+INSERT INTO denree VALUES (14, 'lentille verte', 'du Puy-en-Velay', 'g');
+INSERT INTO denree VALUES (15, 'verveine', '', 'u');
+INSERT INTO denree VALUES (16, 'liqueur de Gentiane', 'marque : Salers', 'u');
+INSERT INTO denree VALUES (17, 'terrines de campagne', 'une flopée de gôuts différents', 'u');
+INSERT INTO denree VALUES (18, 'Bleu', 'd\'Auvergne', 'g');
+
+commit;
+
 -- appartenir_a
+
+INSERT INTO appartenir_a VALUES (1, 2);
+
+commit;
 
 -- contenir
 
