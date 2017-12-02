4dd4a95facbc3b014d66616b43b243fe60b749cd
polochinoc <polochinoc@gmail.com>
2017-12-02 20:07:50 +0100

    fonctions mises à jour

--- fonctions.sql
+++ fonctions.sql
@@ -33,8 +33,22 @@ CREATE OR REPLACE FUNCTION calendrier_livraisons_contrats_adherent(id_adherent I
 RETURNS TABLE(id_contrat INTEGER, date_livraison DATE) STABLE AS
 $$
 SELECT sa.id_contrat, date_livraison FROM souscrire_a sa
+JOIN foyer USING (id_foyer)
+JOIN livraison USING (id_foyer)
+JOIN appartenir_a USING (id_foyer)
+JOIN client USING (id_client)
+WHERE id_client=$1
+ORDER BY date_livraison ASC
+$$ LANGUAGE SQL;
+
+-- Calendrier des livraisons de l'ensemble des contrats d'un foyer donné
+
+CREATE OR REPLACE FUNCTION calendrier_livraisons_contrats_foyer(id_foyer INTEGER)
+RETURNS TABLE(id_contrat INTEGER, date_livraison DATE) STABLE AS
+$$
+SELECT sa.id_contrat, date_livraison FROM souscrire_a sa
 JOIN foyer f USING (id_foyer)
-JOIN livraison l USING (id_foyer)
+JOIN livraison USING (id_foyer)
 WHERE f.id_foyer=$1
 ORDER BY date_livraison ASC
 $$ LANGUAGE SQL;
@@ -53,9 +67,24 @@ GROUP BY f.id_foyer
 ORDER BY n_participations DESC
 $$ LANGUAGE SQL;
 
+-- Somme des montants des tous les contrats souscrits pour chaque foyer
+
+CREATE OR REPLACE FUNCTION somme_montants_contrats()
+RETURNS TABLE(id_adherent INTEGER, somme_contrats BIGINT) STABLE AS
+$$
+SELECT f.id_foyer, SUM(prix_total) AS somme FROM foyer f
+JOIN souscrire_a USING (id_foyer)
+JOIN contrat USING (id_contrat)
+GROUP BY f.id_foyer
+ORDER BY somme DESC
+$$ LANGUAGE SQL;
+
 -- Tests des fonctions --
 
 SELECT liste_foyers_contrat(2);
 SELECT liste_livraisons_mois(11);
 SELECT liste_livraisons_sans_inscriptions();
 SELECT calendrier_livraisons_contrats_adherent(1);
+SELECT calendrier_livraisons_contrats_foyer(1);
+SELECT nombre_participations_annee(2017);
+SELECT somme_montants_contrats();
